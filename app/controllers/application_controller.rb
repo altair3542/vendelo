1731cdd1d4a34516979cd6e8a90adef6d50fbb2e
santiago mesa <thiagomesa21@gmail.com>
2022-09-18 15:05:38 -0500

    traduccion fallida

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  around_action :set_locale
+
+  def set_locale(&action)
+    I18n.with_locale(params[:locale] || I18n.default_locale, &action)
+  end
+
+  private
+
+  def locale_from_header
+    request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+  end
 end
