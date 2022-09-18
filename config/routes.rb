82f07f8aaeac42ec4c67a688a3d5a8a914fb00a4
santiago mesa <thiagomesa21@gmail.com>
2022-09-18 12:14:10 -0500

    agregar nuevo ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    get '/products/new', to: 'products#new', as: :new_product
     get '/products', to: 'products#index'
     get '/products/:id', to: 'products#show', as: :product
+    post '/products', to: 'products#create'
   # Defines the root path route ("/")
   # root "articles#index"
 end
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -17,4 +17,23 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_select '.description', 'ps4 en buen estado'
     assert_select '.price', '150$'
   end
+
+  test 'render a new product form' do
+    get new_product_path
+    assert_response :success
+    assert_select 'form'
+  end
+
+  test 'allow to create a new product' do
+    post products_path, params: {
+      product: {
+        title: 'ps4 slim',
+        description: 'ps4 en buen estado',
+        price: 200
+
+    }
+  }
+    assert_redirected_to /products\/\d+/
+  end
+
 end
