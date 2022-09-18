d09621c0e62924b3a58d3cef2572364237d708b2
santiago mesa <thiagomesa21@gmail.com>
2022-09-18 11:47:10 -0500

    estructura de pruebas

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -4,4 +4,17 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  test 'render a list of products' do
+    get '/products'
+    assert_response :success
+    assert_select '.product', 2
+  end
+
+  test 'render a detailed product page' do
+    get product_path(products(:ps4))
+    assert_response :success
+    assert_select '.title', 'ps4 fat'
+    assert_select '.description', 'ps4 en buen estado'
+    assert_select '.price', '150$'
+  end
 end
