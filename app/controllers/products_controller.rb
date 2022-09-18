89b282ff5f335bd23deeff493e113c47425adf7c
santiago mesa <thiagomesa21@gmail.com>
2022-09-18 13:21:46 -0500

    formulario de editar ok

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -20,10 +20,17 @@ class ProductsController < ApplicationController
     end
   end
 
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+
+  end
+
 
   private
 
   def product_params
     params.require(:product).permit(:title, :description, :price)
   end
-end
+
